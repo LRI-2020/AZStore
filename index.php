ce574b25a2e5fdebc37c30acf73955effcc2589b
LaylaR <layla.richard.2111@gmail.com>
2023-10-25 10:13:59 +0200

    first commit sass folder php files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./assets/scss/style.css">
+    <title>$Title$</title>
+</head>
+<body>
+<div>
+    This is a dummy test
+</div>
+</body>
+</html>
+<?php
